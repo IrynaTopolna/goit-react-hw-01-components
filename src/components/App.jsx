a5facc1eba28f17793ae247ba952698c37c56528
IrynaTopolna <itopolna2808@gmail.com>
2023-02-17 18:55:32 +0200

    Adds components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import { Profile } from './Profile';
+import { Statistics } from './Statistics';
+import { FriendList } from './FriendList';
+import { TransactionHistory } from './TransactionHistory';
+import user from '../user.json';
+import data from '../data.json';
+import friends from '../friends.json';
+import items from '../transactions.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile userData={user} />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={items} />
     </div>
   );
 };
